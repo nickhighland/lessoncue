6917d5f08177edbd64d60d90ab8f44d60023cf9f
nick <nick@nickhighland.com>
2026-07-16 16:05:49 -0400

    fix: organize server settings

--- android-tv/app/build.gradle.kts
+++ android-tv/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.lessoncue.tv"
         minSdk = 26
         targetSdk = 36
-        versionCode = 25
-        versionName = "0.23.0"
+        versionCode = 26
+        versionName = "0.23.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
