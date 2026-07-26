4234f7dd8f3ac48f65d4d58ceb9a54fb1025714c
nick <nick@nickhighland.com>
2026-07-26 00:25:30 -0400

    Fix Android signage manifest test

--- android-tv/app/build.gradle.kts
+++ android-tv/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "org.lessoncue.tv"
         minSdk = 26
         targetSdk = 36
-        versionCode = 50
-        versionName = "0.37.0"
+        versionCode = 51
+        versionName = "0.37.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("boolean", "UPDATE_ENABLED", "false")
         buildConfigField(
