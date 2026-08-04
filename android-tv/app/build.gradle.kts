d4b6e6fa3e1621e44745f0cbf435380b1a900305
nick <nick@nickhighland.com>
2026-08-04 13:24:39 -0400

    Release v0.40.23

--- android-tv/app/build.gradle.kts
+++ android-tv/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "org.lessoncue.tv"
         minSdk = 26
         targetSdk = 36
-        versionCode = 81
-        versionName = "0.40.22"
+        versionCode = 82
+        versionName = "0.40.23"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("boolean", "UPDATE_ENABLED", "false")
         buildConfigField(
