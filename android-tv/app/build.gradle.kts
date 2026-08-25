1c4aa1e03f410c32bce3e1272157a7ba374e7b29
nick <nick@nickhighland.com>
2026-08-25 17:35:19 -0400

    Preserve short-domain forwarding during shortener installs

--- android-tv/app/build.gradle.kts
+++ android-tv/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "org.lessoncue.tv"
         minSdk = 26
         targetSdk = 36
-        versionCode = 128
-        versionName = "0.45.6"
+        versionCode = 129
+        versionName = "0.45.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("boolean", "UPDATE_ENABLED", "false")
         buildConfigField(
