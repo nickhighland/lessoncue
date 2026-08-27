508bd17b035ef340119c6f1b79051bfe2ec1c25a
Nick Highland <nick@nickhighland.com>
2026-08-26 23:05:28 -0400

    Fix Google Play production release publishing (#102)

--- android-tv/app/build.gradle.kts
+++ android-tv/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "org.lessoncue.tv"
         minSdk = 26
         targetSdk = 36
-        versionCode = 137
-        versionName = "0.45.15"
+        versionCode = 138
+        versionName = "0.45.16"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("boolean", "UPDATE_ENABLED", "false")
         buildConfigField(
